3d92271cb6c54ceb4d391356e6e6a0bee12c3f16
Marc <marc34navarro@gmail.com>
2025-08-27 22:36:50 +0200

    tet

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -1,14 +1,14 @@
-output "lambda_exec_role_name" {
-  description = "Nombre del rol de ejecución de Lambda"
-  value       = aws_iam_role.lambda_exec_role.name
-}
+#output "lambda_exec_role_name" {
+#  description = "Nombre del rol de ejecución de Lambda"
+#  value       = aws_iam_role.lambda_exec_role.name
+#}
 
-output "lambda_exec_role_arn" {
-  description = "ARN del rol de ejecución de Lambda"
-  value       = aws_iam_role.lambda_exec_role.arn
-}
+#output "lambda_exec_role_arn" {
+#  description = "ARN del rol de ejecución de Lambda"
+#  value       = aws_iam_role.lambda_exec_role.arn
+#}
 
-output "tiempocolas_policy_arn" {
-  description = "ARN de la policy personalizada TiempoColas"
-  value       = aws_iam_policy.tiempocolas_policy.arn
-}
+#output "tiempocolas_policy_arn" {
+#  description = "ARN de la policy personalizada TiempoColas"
+#  value       = aws_iam_policy.tiempocolas_policy.arn
+#}
--- iac/variables.tf
+++ iac/variables.tf
@@ -1,17 +1,17 @@
 variable "aws_region" {
   description = "Región de AWS para el despliegue"
   type        = string
-  default     = "eu-west-1"
+  default     = "us-east-1"
 }
 
-variable "role_name" {
-  description = "Nombre del rol de ejecución de Lambda"
-  type        = string
-  default     = "lambda-tiempocolas-dev"
-}
+#variable "role_name" {
+#  description = "Nombre del rol de ejecución de Lambda"
+#  type        = string
+#  default     = "lambda-tiempocolas-dev"
+#}
 
-variable "policy_name" {
-  description = "Nombre de la policy IAM"
-  type        = string
-  default     = "TiempoColas-policy-dev"
-}
+#variable "policy_name" {
+#  description = "Nombre de la policy IAM"
+#  type        = string
+#  default     = "TiempoColas-policy-dev"
+#}
